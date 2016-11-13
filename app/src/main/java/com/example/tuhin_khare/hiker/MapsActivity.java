99969926bf556638ebd1807d71596942667a7e3c
Tuhin Khare <tuhinkhare3@gmail.com>
2016-11-13 18:17:05 +0530

    Basic Requirements for the Navigation Drawer

--- app/src/main/java/com/example/tuhin_khare/hiker/MapsActivity.java
+++ app/src/main/java/com/example/tuhin_khare/hiker/MapsActivity.java
@@ -40,7 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));  
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
 }
